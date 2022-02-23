7b73923a8392680d95e9a92f4bb64592e9524085
ismet <ismet@ABRAA5>
2022-02-23 17:31:41 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace Proje5._20WebKamerasındanFotografCekmeveKaydetme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.btnDurdur = new System.Windows.Forms.Button();
+            this.btnFotografCek = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(197, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.AutoSize = true;
+            this.btnBaslat.Location = new System.Drawing.Point(238, 13);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(103, 26);
+            this.btnBaslat.TabIndex = 1;
+            this.btnBaslat.Text = "Başlat";
+            this.btnBaslat.UseVisualStyleBackColor = true;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // btnDurdur
+            // 
+            this.btnDurdur.AutoSize = true;
+            this.btnDurdur.Location = new System.Drawing.Point(372, 13);
+            this.btnDurdur.Name = "btnDurdur";
+            this.btnDurdur.Size = new System.Drawing.Size(98, 26);
+            this.btnDurdur.TabIndex = 2;
+            this.btnDurdur.Text = "Durdur";
+            this.btnDurdur.UseVisualStyleBackColor = true;
+            this.btnDurdur.Click += new System.EventHandler(this.btnDurdur_Click);
+            // 
+            // btnFotografCek
+            // 
+            this.btnFotografCek.AutoSize = true;
+            this.btnFotografCek.Location = new System.Drawing.Point(491, 13);
+            this.btnFotografCek.Name = "btnFotografCek";
+            this.btnFotografCek.Size = new System.Drawing.Size(107, 26);
+            this.btnFotografCek.TabIndex = 3;
+            this.btnFotografCek.Text = "Fotoğraf Çek";
+            this.btnFotografCek.UseVisualStyleBackColor = true;
+            this.btnFotografCek.Click += new System.EventHandler(this.btnFotografCek_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 65);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(775, 373);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnFotografCek);
+            this.Controls.Add(this.btnDurdur);
+            this.Controls.Add(this.btnBaslat);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.Button btnDurdur;
+        private System.Windows.Forms.Button btnFotografCek;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace Proje5._20WebKamerasındanFotografCekmeveKaydetme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private FilterInfoCollection webKamerasiDizisi; //Bilgisayara bağlı kameraları tutan dizi.
+
+        private VideoCaptureDevice webKamerasi; //Kullanılacak kamera cihazı.
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webKamerasiDizisi = new FilterInfoCollection(FilterCategory.VideoInputDevice); //Diziyi doldur.
+            foreach (FilterInfo item in webKamerasiDizisi)
+            {
+                comboBox1.Items.Add(item.Name); //Kamera cihazlarını ekleme.
+            }
+        }
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            webKamerasi = new VideoCaptureDevice(webKamerasiDizisi[comboBox1.SelectedIndex].MonikerString);
+            //Seçili kamer aktif donanım cihazı olarak tanımlanır.
+            webKamerasi.NewFrame += new NewFrameEventHandler(webKamerasi_NewFrame);
+            //Kameranın her bir sahnesinde tetiklenen ve sahnenin PictureBox kontrolünde gösterilmesi için
+            //olay tanımlanır.
+            webKamerasi.Start(); //Kamerayı başlatma.
+        }
+
+        void webKamerasi_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bmp = (Bitmap)eventArgs.Frame.Clone();
+            //Görüntü tanımlama ve PictureBox kontrolüne atama.
+            pictureBox1.Image = bmp;
+        }
+        private void btnDurdur_Click(object sender, EventArgs e)
+        {
+            if(webKamerasi.IsRunning)
+            {
+                webKamerasi.Stop(); //Kamerayı durdurma.
+            }
+        }
+
+        private void btnFotografCek_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog svf = new SaveFileDialog();
+            DialogResult dialog = svf.ShowDialog();
+            if(dialog == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(svf.FileName);
+            }
+        }
+    }
+}
